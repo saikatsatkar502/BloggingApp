1934f19c4300fa0ef791eee97ac747b31c2746b0
soumik sarkar <saikatsarkar502@gmail.com>
2022-09-23 12:23:34 +0530

    media Type added.

--- BloggingApplicationBackend/src/main/java/com/blogapp/backend/config/SecurityConfig.java
+++ BloggingApplicationBackend/src/main/java/com/blogapp/backend/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.blogapp.backend.config;
 import com.blogapp.backend.security.CustomeUserDetailsService;
 import com.blogapp.backend.security.JWTauthenticationEntryPoint;
 import com.blogapp.backend.security.JwtAuthenticationFilter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
